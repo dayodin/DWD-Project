59b12643a46e7802303769e65ff5bc9ddc029ef2
Karsten Dinsmore <kddinsmore@outlook.com>
2025-03-21 16:55:21 -0700

    changed index to include staticDir

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,6 +8,7 @@ import { registerShareRoutes } from "./routes/share";
 
 dotenv.config(); // Read the .env file in the current working directory, and load values into process.env.
 const PORT = process.env.PORT || 3000;
+const staticDir = process.env.STATIC_DIR || "public";
 
 const { MONGO_USER, MONGO_PWD, MONGO_CLUSTER, DB_NAME } = process.env;
 
@@ -24,6 +25,7 @@ async function setUpServer () {
     const app = express();
 
     app.use(express.json());
+    app.use(express.static(staticDir));
 
     app.use("/api/*", verifyAuthToken)
 
